ea4bd6419131f4c01d085959e1cfebbcb9fde4b7
wegoagain <tsdevelops00@gmail.com>
2025-06-30 00:18:44 +0100

    private subnet added

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 
 ![alt text](./three-tier-architecture.drawio.png)
 
+## Prerequisites (I recommend researching all of these if needed)
+
+1. Install AWS CLI (configured with credentials)
+2. Add IAM permissions for new user account from root (good practice) - I gave Administrator access
+3. Install Terraform 
+4. SSH key (for connecting to EC2)
+
 ---
 when deleting terraform and creating new one - either. change name of secretsmanager(new name) or force delete using below
 as it dont allow you to create it again due to policy
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -57,6 +57,31 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+# Route table for private subnets
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = "three-tier-private-route-table"
+  }
+}
+
+# Associate private subnets with private route table
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+
+
+
 resource "aws_security_group" "frontend_sg" {
   vpc_id = aws_vpc.main.id
   ingress {
